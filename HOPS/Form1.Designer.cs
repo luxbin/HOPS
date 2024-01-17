726579d45e505cbd1999382a60ea58a54f196f5d
lushofbit <fliudmyla1@gmail.com>
2024-01-17 07:36:54 +0200

    initial commit

---
+++ HOPS/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace HOPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tvMain = new System.Windows.Forms.TreeView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSavePath = new System.Windows.Forms.TextBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lvImage = new System.Windows.Forms.ListView();
+            this.colNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtConv = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLRD = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSiteName = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tvMain
+            // 
+            this.tvMain.AllowDrop = true;
+            this.tvMain.FullRowSelect = true;
+            this.tvMain.HideSelection = false;
+            this.tvMain.Location = new System.Drawing.Point(7, 137);
+            this.tvMain.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.tvMain.Name = "tvMain";
+            this.tvMain.Size = new System.Drawing.Size(405, 384);
+            this.tvMain.TabIndex = 0;
+            this.tvMain.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvMain_AfterSelect);
+            this.tvMain.Click += new System.EventHandler(this.tvMain_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 536);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Save Path";
+            // 
+            // txtSavePath
+            // 
+            this.txtSavePath.AllowDrop = true;
+            this.txtSavePath.Enabled = false;
+            this.txtSavePath.Location = new System.Drawing.Point(358, 536);
+            this.txtSavePath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSavePath.Name = "txtSavePath";
+            this.txtSavePath.Size = new System.Drawing.Size(330, 29);
+            this.txtSavePath.TabIndex = 10;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(694, 536);
+            this.btnBrowser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(40, 32);
+            this.btnBrowser.TabIndex = 11;
+            this.btnBrowser.Text = "...";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(749, 533);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 37);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HOPS.Properties.Resources.Color_logo_with_background;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 127);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lvImage
+            // 
+            this.lvImage.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNo,
+            this.colName});
+            this.lvImage.FullRowSelect = true;
+            this.lvImage.GridLines = true;
+            this.lvImage.HideSelection = false;
+            this.lvImage.Location = new System.Drawing.Point(419, 137);
+            this.lvImage.Name = "lvImage";
+            this.lvImage.Size = new System.Drawing.Size(405, 384);
+            this.lvImage.TabIndex = 17;
+            this.lvImage.UseCompatibleStateImageBehavior = false;
+            this.lvImage.View = System.Windows.Forms.View.Details;
+            // 
+            // colNo
+            // 
+            this.colNo.Text = "No";
+            this.colNo.Width = 43;
+            // 
+            // colName
+            // 
+            this.colName.Text = "File name";
+            this.colName.Width = 342;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtConv);
+            this.groupBox1.Controls.Add(this.txtCategory);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtLRD);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtSiteName);
+            this.groupBox1.Location = new System.Drawing.Point(207, -5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(617, 135);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 24);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Convert";
+            // 
+            // txtConv
+            // 
+            this.txtConv.Enabled = false;
+            this.txtConv.Location = new System.Drawing.Point(117, 90);
+            this.txtConv.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtConv.Name = "txtConv";
+            this.txtConv.Size = new System.Drawing.Size(480, 29);
+            this.txtConv.TabIndex = 22;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Enabled = false;
+            this.txtCategory.Location = new System.Drawing.Point(117, 53);
+            this.txtCategory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(480, 29);
+            this.txtCategory.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 24);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Category";
+            // 
+            // txtLRD
+            // 
+            this.txtLRD.Location = new System.Drawing.Point(342, 17);
+            this.txtLRD.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtLRD.Name = "txtLRD";
+            this.txtLRD.Size = new System.Drawing.Size(77, 29);
+            this.txtLRD.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "LRD";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Site Name";
+            // 
+            // txtSiteName
+            // 
+            this.txtSiteName.Location = new System.Drawing.Point(117, 18);
+            this.txtSiteName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSiteName.Name = "txtSiteName";
+            this.txtSiteName.Size = new System.Drawing.Size(167, 29);
+            this.txtSiteName.TabIndex = 16;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 538);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 24);
+            this.lblStatus.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 584);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lvImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.txtSavePath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tvMain);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "HOPS";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvMain;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSavePath;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView lvImage;
+        private System.Windows.Forms.ColumnHeader colNo;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtConv;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLRD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSiteName;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ HOPS/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HOPS
+{
+    public partial class Form1 : Form
+    {
+        HopsNode curNode = null;
+        string savePath = "";
+        string[] imageFileList = null;
+        int curFileIndex = 0;
+
+        string categoryXml = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            /*
+            var doc = XDocument.Load(@"c:\temp\testxml.xml");
+            foreach (var element in doc.Elements("Products").Elements())
+            {
+                Console.WriteLine(element.Attribute("category"));
+            }
+            */
+
+            InitControls();
+        }
+
+        private void InitControls()
+        {
+            LoadXml();
+        }
+
+        private void LoadXml()
+        {
+            var path = Directory.GetCurrentDirectory() + "\\sub.xml";
+
+            TreeNode root = new TreeNode();
+
+            root.Name = "root";
+            root.Text = "/";
+            HopsNode tagRoot = new HopsNode();
+            tagRoot.type = "root";
+            root.Tag = tagRoot;
+
+            tvMain.Nodes.Add(root);
+
+            Console.WriteLine(path);
+
+            var doc = XDocument.Parse(HOPS.Properties.Resources.sub);
+            //var doc = XDocument.Load(path);
+
+            foreach (var eleMain in doc.Elements("Root").Elements())
+            {
+                //Console.WriteLine(element.Attribute("category"));
+                var txt = eleMain.Attribute("text").Value;
+                TreeNode main = new TreeNode();
+                main.Name = txt;
+                main.Text = txt;
+
+                HopsNode tag = new HopsNode();
+                tag.type = "main";
+                tag.text = txt;
+                main.Tag = tag;
+                root.Nodes.Add(main);
+
+                foreach(var eleSub in eleMain.Elements())
+                {
+                    txt = eleSub.Attribute("text").Value;
+                    TreeNode sub = new TreeNode();
+                    sub.Name = txt;
+                    sub.Text = txt;
+
+                    HopsNode tag1 = new HopsNode();
+                    tag1.type = "sub";
+                    tag1.conv = eleSub.Attribute("conv").Value;
+                    tag1.taken = eleSub.Attribute("taken").Value;
+                    tag1.text = txt;
+                    sub.Tag = tag1;
+                    main.Nodes.Add(sub);
+                }
+            }
+
+            tvMain.ExpandAll();
+        }
+
+        private void AddImagesToList()
+        {
+            lvImage.Items.Clear();
+
+            /*
+            string sitePath = savePath + "//" + txtSiteName.Text;
+            Directory.CreateDirectory(sitePath);
+
+            HopsNode parentNode = (HopsNode)tvMain.SelectedNode.Parent.Tag;
+
+            sitePath += "//" + parentNode.text;
+
+            Directory.CreateDirectory(sitePath);
+
+            string lastPath = curNode.conv.Replace("XXXX_", txtLRD.Text + "_");
+
+            string destPath = sitePath + "//" + lastPath;
+            Directory.CreateDirectory(destPath);
+
+            int nIndex = Directory.GetFiles(destPath).Length + 1;
+            */
+
+            int nIndex = 1;
+
+            foreach (string file in imageFileList)
+            {
+                string[] items = new string[2];
+                items[0] = nIndex.ToString();
+                items[1] = Path.GetFileName(file);
+                ListViewItem item = new ListViewItem(items);
+                lvImage.Items.Add(item);
+
+                nIndex ++;
+            }
+
+            lblStatus.Text = imageFileList.Length.ToString() + " file selected.";
+        }
+
+        private void txtImagePath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            imageFileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            string s = "";
+
+            foreach (string file in imageFileList)
+            {
+                FileAttributes attr = File.GetAttributes(file);
+                bool isFolder = (attr & FileAttributes.Directory) == FileAttributes.Directory;
+                s = s + " " + file;
+            }
+
+            AddImagesToList();
+        }
+
+        private void tvMain_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tvMain_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            curNode = (HopsNode)tvMain.SelectedNode.Tag;
+
+            if (curNode.type != "sub")
+            {
+                txtCategory.Text = "";
+                txtConv.Text = "";
+            }
+            else
+            {
+                txtCategory.Text = curNode.text;
+                txtConv.Text = curNode.conv;
+            }
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtSavePath.Text = folderDlg.SelectedPath;
+                savePath = folderDlg.SelectedPath;
+                //Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string siteName = txtSiteName.Text;
+            string LRD = txtLRD.Text;
+
+            if (siteName == "")
+            {
+                MessageBox.Show("Please input Site Name.");
+                txtSiteName.Focus();
+                return;
+            }
+
+            if (LRD == "")
+            {
+                MessageBox.Show("Please input LRD.");
+                txtLRD.Focus();
+                return;
+            }
+
+            if (txtCategory.Text == "")
+            {
+                MessageBox.Show("Please select category.");
+                return;
+            }
+
+            if (lvImage.Items.Count == 0)
+            {
+                MessageBox.Show("Please drag and drop image files.");
+                return;
+            }
+
+            if (txtSavePath.Text == "")
+            {
+                MessageBox.Show("Please select save path.");
+                btnBrowser.Focus();
+                return;
+            }
+
+            string sitePath = savePath + "//" + siteName;
+            Directory.CreateDirectory(sitePath);
+
+            HopsNode parentNode = (HopsNode)tvMain.SelectedNode.Parent.Tag;
+
+            sitePath += "//" + parentNode.text;
+
+            Directory.CreateDirectory(sitePath);
+
+            string lastPath = curNode.conv.Replace("XXXX_", LRD + "_");
+
+            string destPath = sitePath + "//" + lastPath;
+            Directory.CreateDirectory(destPath);
+
+            int index = Directory.GetFiles(destPath).Length + 1;
+
+
+            foreach (string srcFilePath in imageFileList)
+            {
+                FileAttributes attr = File.GetAttributes(srcFilePath);
+                bool isFolder = (attr & FileAttributes.Directory) == FileAttributes.Directory;
+                if (isFolder) continue;
+
+                //string destFileName = Path.GetFileNameWithoutExtension(srcFilePath);
+
+                string destFileName = lastPath + index.ToString("00"); // "D2"
+
+                string destFilePath = destPath;
+
+                destFilePath += "//" + destFileName + Path.GetExtension(srcFilePath);
+
+                File.Copy(srcFilePath, destFilePath, true);
+
+                index++;
+            }
+
+            lblStatus.Text = imageFileList.Length.ToString() + " file copied.";
+
+            lvImage.Items.Clear();
+        }
+    }
+
+    public class HopsNode
+    {
+        public string type = "";
+        public string conv = "";
+        public string taken = "";
+        public string text = "";
+    }
+}
